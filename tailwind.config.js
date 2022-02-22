c2b394beb113387b0ca724c45fa7a3b4cf9cf1e3
Katja Levring <katj6233@stud.kea.dk>
2022-02-22 10:08:05 +0100

    indsætning af farver og fonte, og oprydning i html'er

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,32 @@
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      /*sort*/
+      primary: "#000000",
+      /*hvid*/
+      secondary: "#ffffff",
+      /*gul*/
+      third: "#F1F500",
+      /*blå*/
+      fourth: "#0500FF",
+      /*pink*/
+      fifth: "#FF01D6",
+      /*grøn*/
+      sixth: "#00B412",
+      /*rød*/
+      seventh: "#FF0000",
+      /*orange*/
+      eighth: "#FD8900",
+      /*lilla*/
+      ninth: "#9800F5",
+    },
+
+    fontFamily: {
+      jetbrains: "'JetBrains Mono', monospace;",
+    },
+
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
